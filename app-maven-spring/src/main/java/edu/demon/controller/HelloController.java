00ed74d83db247836cc7e51ecfc332f0217450a2
jflsdemon <497725134@qq.com>
2017-11-26 21:47:33 +0800

    comment like last commit, last commit did not add app-maven-spring project

---
+++ app-maven-spring/src/main/java/edu/demon/controller/HelloController.java
@@ -0,0 +1,17 @@
+package edu.demon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	@RequestMapping("/hello")
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("spring", "spring mvc");
+		mv.setViewName("hello");
+		return mv;
+	}
+}
